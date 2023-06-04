f2bbbedf05cfaae323bc9dcc4e64c9bad0acbec9
luunhatthanhcnttk61 <6151071101@st.utc2.edu.vn>
2023-06-04 22:40:40 +0700

    add Helper.cs and create Collection view

---
+++ csharp_real_estate/Models/ProductViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace csharp_real_estate.Models
+{
+    public class ProductViewModels
+    {
+        public product product { get; set; }
+        public string thumb { get; set; }
+        public ProductViewModel()
+        {
+            // Empty Constructor
+        }
+        public ProductViewModel(product product, string thumb)
+        {
+            this.product = product;
+            this.thumb = thumb;
+        }
+    }
+}
